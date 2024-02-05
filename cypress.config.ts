ef2ef134098272fbc531d85dcb5b73889c2f5eb4
Patrycja Pawlowska <patriss93@gmail.com>
2024-02-05 13:23:34 +0100

    "cypress-grep" configuration set

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/grep/src/plugin')(config);
+      return config;
     },
     baseUrl: 'https://automationteststore.com/'
   },
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -23,4 +23,10 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     return false
-  })
+});
+
+// cypress/support/index.js
+// load and register the grep feature using "require" function
+// https://github.com/cypress-io/cypress/tree/develop/npm/grep
+const registerCypressGrep = require('@cypress/grep')
+registerCypressGrep()
